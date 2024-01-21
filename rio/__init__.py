d107420c3c4d03c1cc8f847a91f3cb2594e5c3b8
KR <79979949+justfoolingaround@users.noreply.github.com>
2024-01-21 08:53:21 +0545

    Chore: Add bisect and logging imports Chore: Refactor ExistingRanges.add method Feat: Add fmt_function_call and log_member_function_activity decorators

--- rio/__init__.py
+++ rio/__init__.py
@@ -1,5 +1,6 @@
 import io
 import logging
+import tempfile
 import typing as t
 from warnings import warn
 
@@ -15,68 +16,49 @@ class RemoteIO(io.IOBase):
         self,
         *args,
         session: t.Optional[requests.Session] = None,
-        buffer: t.Optional[t.BinaryIO] = None,
+        buffer: t.Optional[io.BytesIO] = None,
         **kwargs,
     ):
-        self.session: requests.Session = session or requests.Session()
+        self.session = session or requests.Session()
         self.request_args = args
-
-        self.headers: t.Dict[str, str] = kwargs.pop("headers", {})
+        self.headers = kwargs.pop("headers", {})
         self.request_kwargs = kwargs
-
         self.pos = 0
-
-        self.buffer = buffer or io.BytesIO()
+        self.buffer = buffer or tempfile.SpooledTemporaryFile()
         self.existing_ranges = ExistingRanges()
-
-        self.streaming_response: t.Optional[requests.Response] = None
-
-        self.server_read: int = 0
-        self.server_full_size: t.Optional[int] = None
+        self.streaming_response = None
+        self.server_read = 0
+        self.server_full_size = None
 
         self.logger = logging.getLogger("remoteio")
 
-    def seek(self, offset: int, whence: WHENCE_TYPE = 0):
+    def seek(self, offset: int, whence: WHENCE_TYPE = io.SEEK_SET):
         assert whence in (0, 1, 2), "Invalid whence"
-
         initial = self.pos
 
-        if whence == 0:
-            if self.pos == offset and self.streaming_response is not None:
-                self.logger.info(
-                    f"Already at position {self.pos}; not attempting to seek at all."
-                )
-                return self.pos
-
+        if whence == io.SEEK_SET:
             self.pos = offset
-            hypothetical_range = (self.pos, self.pos + 1)
+        elif whence == io.SEEK_CUR:
+            self.pos += offset
+            offset = self.pos
+        elif whence == io.SEEK_END:
+            total_size = self.attempt_size_resolving()
+            if total_size is None:
+                raise ValueError("Cannot seek from EOF when the size is unknown")
+            self.pos = total_size + offset
+            if offset == 0:
+                return self.pos
         else:
-            if whence == 1:
-                self.pos += offset
-                offset = self.pos
-                hypothetical_range = (self.pos, self.pos + 1)
-            else:
-                if whence == 2:
-                    offset = -abs(offset)
-
-                    total_size = self.attempt_size_resolving()
-
-                    if total_size is None:
-                        raise ValueError(
-                            "Cannot seek from EOF when the size is unknown"
-                        )
-                    self.pos = total_size + offset
-                    hypothetical_range = (total_size + offset, total_size)
-                else:
-                    raise ValueError("Invalid whence, must be 0, 1 or 2")
+            raise ValueError("Invalid whence, must be 0, 1 or 2")
 
         if self.streaming_response is not None:
             if initial == self.pos:
                 self.logger.info(
-                    f"Already at position {self.pos}; not opening an unnecessary stream."
+                    f"Already at position {self.pos}; the internal state is constant."
                 )
                 return self.pos
 
+            hypothetical_range = (self.pos, self.pos + 1)
             if hypothetical_range in self.existing_ranges:
                 self.logger.info(
                     f"Expected data at the range {hypothetical_range} is already in the buffer; not opening an unnecessary stream."
@@ -88,75 +70,61 @@ class RemoteIO(io.IOBase):
             )
             self.streaming_response.close()
 
+        byte_range = f"{offset}"
+
         if whence <= 1:
-            byte_range = f"{offset}-"
-        else:
-            byte_range = f"-{abs(offset)}"
+            byte_range += "-"
 
-        self.logger.info(f"Requesting byte range: {byte_range} from the server.")
-        self.streaming_response = self.session.request(
-            *self.request_args,
-            headers=self.headers | {"Range": f"bytes={byte_range}"},
-            **self.request_kwargs,
-            stream=True,
-        )
-
+        self.streaming_response = self.request_stream(byte_range)
         return self.pos
 
     def read(self, n: int = -1):
         if self.pos == 0 and not n + 1:
             warn(
-                "The RIO object is being read in full which is the worst case for its usage. "
-                "Using packages that don't try to read the entire file is highly recommended."
+                "The RIO object is being read in full which is the worst case for its usage. Using packages that don't try to read the entire file is highly recommended."
             )
 
         file_size = self.attempt_size_resolving()
+        start = self.pos
+        end = (self.pos + n) if n + 1 else file_size
 
         partitions = (
-            self.existing_ranges.iter_partition(
-                self.pos,
-                (self.pos + n) if n + 1 else file_size,
-            )
+            self.existing_ranges.iter_partition(start, end)
             if file_size is not None
-            else ((self.pos, (self.pos + n) if n + 1 else None),)
+            else [(start, end)]
         )
 
         chunk = b""
-
         initial_position = self.pos
 
-        for start, end in partitions:
-            self.buffer.seek(start)
-            partition_n = (end - start) if end is not None else -1
+        self.logger.info(
+            f"Requested data at the range {start}-{end} ({end-start} bytes) from the server."
+        )
 
-            if (start, end) in self.existing_ranges:
+        for part_start, part_end in partitions:
+            self.buffer.seek(part_start)
+            partition_n = part_end - part_start if part_end is not None else -1
+
+            if (part_start, part_end) in self.existing_ranges:
                 self.logger.info(
-                    f"Reading data at the range {start}-{end} from existing buffer."
+                    f"Reading data at the range {part_start}-{part_end} from existing buffer."
                 )
                 chunk += self.buffer.read(partition_n)
-
             else:
-                self.seek(start)
-
+                self.seek(part_start)
                 if self.streaming_response is None:
                     raise RuntimeError("The stream was not open after seeking")
 
-                server_chunk: bytes = self.streaming_response.raw.read(partition_n)
-
-                size_of_chunk = len(server_chunk)
-
-                if partition_n != -1 and size_of_chunk != partition_n:
-                    warn(
-                        f"Expected {partition_n} bytes, got {size_of_chunk} bytes at {start}-{end if end is not None else ''}."
-                    )
+                self.logger.info(
+                    f"Reading data at the range {part_start}-{part_end} from remote."
+                )
+                server_chunk = self.read_server_chunk(partition_n)
 
                 self.server_read += self.buffer.write(server_chunk)
-                self.existing_ranges.add(start, start + len(server_chunk))
-
+                self.existing_ranges.add(part_start, part_start + len(server_chunk))
                 chunk += server_chunk
 
         self.pos = initial_position + len(chunk)
-
         return chunk
 
     def tell(self):
@@ -186,53 +154,36 @@ class RemoteIO(io.IOBase):
         if self.server_full_size is not None:
             return self.server_full_size
 
-        if self.streaming_response is not None:
-            headers = self.streaming_response.headers
+        if self.streaming_response is None:
+            self.streaming_response = self.request_stream("0-")
 
-            if "Content-Range" in headers:
-                self.server_full_size = int(headers["Content-Range"].split("/")[1])
-
-                self.logger.info(
-                    f"Size of the file was resolved via an existing stream: {self.server_full_size}"
-                )
-
-                return self.server_full_size
-
-        kwargs = self.request_kwargs.copy()
-
-        if kwargs.pop("method", None) is None:
-            args = self.request_args[1:]
-        else:
-            args = self.request_args
-
-        response_headers = self.session.head(
-            *args, **kwargs, headers=self.headers, allow_redirects=True
-        ).headers
-
-        content_length = int(response_headers.get("Content-Length", 0))
-
-        if content_length:
-            self.server_full_size = content_length
-            self.logger.info(
-                f"Size of the file was resolved via a HEAD request: {self.server_full_size}"
-            )
-
-            return self.server_full_size
-
-        headers = self.headers.copy()
-        headers["Range"] = "bytes=0-"
-
-        nc_response = self.session.get(*args, **kwargs, headers=headers, stream=True)
-
-        if "Content-Range" in nc_response.headers:
+        if "Content-Range" in self.streaming_response.headers:
             self.server_full_size = int(
-                nc_response.headers["Content-Range"].split("/")[1]
+                self.streaming_response.headers["Content-Range"].split("/")[1]
             )
-
             self.logger.info(
-                f"Size of the file was resolved via a non-conventional streaming method: {self.server_full_size}"
+                f"Size of the file was resolved via an existing stream: {self.server_full_size}"
             )
-            nc_response.close()
             return self.server_full_size
 
         return None
+
+    def request_stream(self, byte_range):
+        self.logger.info(f"Requesting byte range: {byte_range} from the server.")
+        response = self.session.request(
+            *self.request_args,
+            headers=self.headers | {"Range": f"bytes={byte_range}"},
+            **self.request_kwargs,
+            stream=True,
+        )
+        return response
+
+    def read_server_chunk(self, partition_n):
+        server_chunk = self.streaming_response.raw.read(partition_n)
+        size_of_chunk = len(server_chunk)
+
+        if partition_n != -1 and size_of_chunk != partition_n:
+            warn(
+                f"Expected {partition_n} bytes, got {size_of_chunk} bytes at {self.pos}-{self.pos + size_of_chunk}."
+            )
+        return server_chunk
--- rio/utils.py
+++ rio/utils.py
@@ -1,4 +1,7 @@
+import bisect
 import functools
+import logging
+import time
 import typing as t
 
 
@@ -7,50 +10,85 @@ class ExistingRanges:
         self.existing_ranges: t.List[t.Tuple[int, int]] = list()
 
     def add(self, start: int, end: int):
-        if (start, end) in self.existing_ranges:
-            return
+        if (start, end) not in self.existing_ranges:
+            bisect.insort_right(self.existing_ranges, (start, end))
 
-        if start > end:
-            start, end = end, start
+            index = 0
 
-        self.existing_ranges.append((start, end))
-        self.existing_ranges = self.merge(self.existing_ranges)
+            for start, end in self.existing_ranges[1:]:
+                if self.existing_ranges[index][1] >= start:
+                    self.existing_ranges[index] = (
+                        self.existing_ranges[index][0],
+                        max(self.existing_ranges[index][1], end),
+                    )
+                else:
+                    index += 1
+                    self.existing_ranges[index] = (start, end)
 
-    @staticmethod
-    def merge(existing_ranges: list):
-        if len(existing_ranges) <= 1:
-            return existing_ranges
-
-        intervals = existing_ranges.copy()
-        intervals.sort(key=lambda interval: interval[0])
-
-        result = list((intervals.pop(0),))
-
-        for start_i, end_i in intervals:
-            edge_interval_start, edge_interval_end = result[-1]
-            if start_i <= edge_interval_end + 1:
-                result[-1] = edge_interval_start, max(edge_interval_end, end_i)
-            else:
-                result.append((start_i, end_i))
-
-        return result
+            self.existing_ranges = self.existing_ranges[: index + 1]
 
     def iter_partition(self, start: int, end: int):
-        (*partitioners,) = (
-            _
-            for _ in functools.reduce(tuple.__add__, self.existing_ranges, ())
-            if start < _ < end
-        )
+        partitioners = [
+            partition
+            for partition in functools.reduce(tuple.__add__, self.existing_ranges, ())
+            if start < partition < end
+        ]
 
         if partitioners:
-            yield from zip((start, *partitioners, end), (*partitioners, end))
+            yield from zip([start, *partitioners, end], [*partitioners, end])
         else:
             yield (start, end)
 
     def __contains__(self, other):
-        (start, end) = other
+        start, end = other
 
         return any(
             starting <= start <= ending and starting <= end <= ending
             for starting, ending in self.existing_ranges
         )
+
+
+def fmt_function_call(name, args, kwargs):
+    if kwargs:
+        fmt_kwargs = {
+            ", ".join([f"{key}={repr(value)}" for key, value in kwargs.items()])
+        }
+    else:
+        fmt_kwargs = ""
+
+    if args:
+        fmt_args = ", ".join([repr(arg) for arg in args])
+
+    else:
+        fmt_args = ""
+
+    if fmt_args and fmt_kwargs:
+        fmt_args += ", "
+
+    return f"{name}({fmt_args}{fmt_kwargs})"
+
+
+def log_member_function_activity(activity_name=None, nano_precision=False):
+    time_func = time.perf_counter_ns if nano_precision else time.perf_counter
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            if hasattr(self, "logger"):
+                logger = self.logger
+            else:
+                logger = logging.getLogger("activity_logger")
+
+            start = time_func()
+            result = func(self, *args, **kwargs)
+
+            name = f"{self.__class__.__name__}.{activity_name or func.__name__}"
+
+            logger.info(
+                f"{fmt_function_call(name, args, kwargs)} took {time_func() - start} {'nano' if nano_precision else ''}seconds"
+            )
+            return result
+
+        return wrapper
+
+    return decorator
